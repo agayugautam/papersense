5c41917e5d559782573662b96b33abdbaf08880b
Ayush Gautam <60062058+agayugautam@users.noreply.github.com>
2026-01-31 10:35:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ import uvicorn
 from fastapi import FastAPI
 from database import Base, engine
 from routes import documents, dashboard, search
+from fastapi.middleware.cors import CORSMiddleware
+
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="PaperSense API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(documents.router, prefix="/api/documents")
 app.include_router(dashboard.router, prefix="/api/dashboard")
