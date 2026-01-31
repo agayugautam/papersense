f2a507dc3208e98f29fa4296f17c9a2f31051729
Ayush Gautam <60062058+agayugautam@users.noreply.github.com>
2026-01-31 10:06:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # main.py
+import os
+import uvicorn
 from fastapi import FastAPI
 from database import Base, engine
 from routes import documents, dashboard, search
@@ -14,3 +16,8 @@ app.include_router(search.router, prefix="/api/search")
 @app.get("/")
 def health():
     return {"status": "PaperSense backend running"}
+
+# THIS PART IS CRITICAL FOR RAILWAY
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
