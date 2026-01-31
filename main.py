702565a8a0ac92ef876bb5315dfc5fa2ca18de9a
Fearless2512-lang <ayushgautam.dm251023@greatlakes.edu.in>
2026-01-31 15:33:40 +0530

    Commit 2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="PaperSense API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],   # allows localhost + deployed frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
