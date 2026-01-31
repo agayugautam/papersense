1a540457a910fbabf488f310ba64c960ed3d55b8
Fearless2512-lang <ayushgautam.dm251023@greatlakes.edu.in>
2026-01-31 15:52:47 +0530

    Commit 3

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, Text, DateTime
+from sqlalchemy.sql import func
 from database import Base
 
 class Document(Base):
     __tablename__ = "documents"
 
-    id = Column(Integer, primary_key=True, index=True)
-    filename = Column(String, index=True)
-    document_type = Column(String, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    filename = Column(String)
+    document_type = Column(String)
     size_mb = Column(Float)
-    content = Column(String)
+    content = Column(Text)
+    uploaded_at = Column(DateTime(timezone=True), server_default=func.now())
