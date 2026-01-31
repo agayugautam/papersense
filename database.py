5e9aca43ff79d1069311c64dafffd4339efd9cc4
Ayush Gautam <60062058+agayugautam@users.noreply.github.com>
2026-01-31 17:41:29 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,10 +4,10 @@ from config import DATABASE_URL
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
 )
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
