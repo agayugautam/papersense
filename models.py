434941c6c1bada415235842b8c1474b7697d8f1f
Ayush Gautam <60062058+agayugautam@users.noreply.github.com>
2026-01-31 14:20:34 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, DateTime
 from database import Base
+from datetime import datetime
 
 class Document(Base):
     __tablename__ = "documents"
 
     id = Column(Integer, primary_key=True, index=True)
     filename = Column(String)
-    url = Column(String)
-    document_type = Column(String)
+    blob_url = Column(String)
+    document_type = Column(String, default="Other")
     size_mb = Column(Float)
+    created_at = Column(DateTime, default=datetime.utcnow)
