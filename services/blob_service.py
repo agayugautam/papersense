d2e60f91c632660a98a8419d21bd4a48aab938fe
Ayush Gautam <60062058+agayugautam@users.noreply.github.com>
2026-01-31 17:42:27 +0530

    Update blob_service.py

--- services/blob_service.py
+++ services/blob_service.py
@@ -1,22 +1,14 @@
 from azure.storage.blob import BlobServiceClient
 from config import AZURE_STORAGE_CONNECTION_STRING, AZURE_BLOB_CONTAINER
+import uuid
 
-def upload_to_azure(filename: str, data: bytes) -> str:
-    print("AZURE UPLOAD:", filename)
+blob_service = BlobServiceClient.from_connection_string(
+    AZURE_STORAGE_CONNECTION_STRING
+)
+container = blob_service.get_container_client(AZURE_BLOB_CONTAINER)
 
-    blob_service = BlobServiceClient.from_connection_string(
-        AZURE_STORAGE_CONNECTION_STRING
-    )
-    container = blob_service.get_container_client(AZURE_BLOB_CONTAINER)
-
-    try:
-        container.create_container()
-    except:
-        pass
-
-    blob = container.get_blob_client(filename)
-    blob.upload_blob(data, overwrite=True)
-
-    print("AZURE UPLOAD SUCCESS")
-
-    return f"{AZURE_BLOB_CONTAINER}/{filename}"
+def upload_to_azure(file_bytes, filename):
+    blob_name = f"{uuid.uuid4()}_{filename}"
+    blob_client = container.get_blob_client(blob_name)
+    blob_client.upload_blob(file_bytes, overwrite=True)
+    return blob_client.url
